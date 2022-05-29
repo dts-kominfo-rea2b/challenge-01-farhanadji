74ff86ac79a410c7f06f24624034628f0d776653
Farhan Adji <farhanadji@live.com>
2022-05-29 15:50:27 +0700

    Add 2 users

--- index.js
+++ index.js
@@ -1,11 +1,88 @@
-// TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const monicaFavoriteColor = Array.from(
+    new Set([
+        "Yellow", "Pink", "White", "Purple"
+    ])
+);
 
-// TODO: Masukkan hasil akhir dari kedua user di sini
+const monicaFavoriteRestaurant = Array.from(
+    new Set([
+        "Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"
+    ])
+);
+
+const wendyFavoriteColor =  Array.from(
+    new Set([
+        "Blue", "Black", "Grey"
+    ])
+);
+
+const wendyFavoriteRestaurant = Array.from(
+    new Set([
+        "Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"
+    ])
+);
+
+const firstUser = {
+    name: "Monica",
+    gender: "Female",
+    age: 17,
+    email: "monica@dingdong.com",
+    favoriteColor: monicaFavoriteColor,
+    isHavePet: true,
+    education: [
+        {
+            name: "SD 01",
+            city: "Jakarta",
+            graduate: 2016
+        },
+        {
+            name: "SMP 02",
+            city: "Jakarta",
+            graduate: 2019
+        },
+        {
+            name: "SMA 03",
+            city: "Tangerang"
+        }
+    ],
+    favoriteRestaurant: monicaFavoriteRestaurant
+};
+const secondUser = {
+    name: "Wendy",
+    gender: "Male",
+    age: 23,
+    email: "wendy@dingdong.com",
+    favoriteColor: wendyFavoriteColor,
+    isHavePet: false,
+    education: [
+        {
+            name: "SD 02",
+            city: "Jakarta",
+            graduate: 2010
+        },
+        {
+            name: "SMP 03",
+            city: "Bogor",
+            graduate: 2013
+        },
+        {
+            name: "SMA 01",
+            city: "Surabaya",
+            graduate: 2016
+        },
+        {
+            name: "Universitas Maju",
+            city: "Tangerang"
+        }
+    ],
+    favoriteRestaurant: wendyFavoriteRestaurant
+};
+
 const users = [];
+users.push(firstUser);
+users.push(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
